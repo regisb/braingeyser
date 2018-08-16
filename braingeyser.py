b075b9c7e8911f9d3ed855dc03d4b15783661b46
Régis Behmo <regis@behmo.com>
2018-08-16 14:39:43 +0200

    Rollback mkv support which exists on chrome

--- braingeyser.py
+++ braingeyser.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 class Videos:
     ROOT_DIR = os.environ.get('VIDEO_ROOT_DIR', '/tmp')
-    SUPPORTED_EXTENSIONS = ['.mp4', '.avi']
+    SUPPORTED_EXTENSIONS = ['.mp4', '.avi', '.mkv']
     SUPPORTED_SUBTITLES_EXTENSIONS = ['.srt', '.vtt']
 
 
